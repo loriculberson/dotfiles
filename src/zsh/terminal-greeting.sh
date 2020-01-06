55fdc6a083dd34217887f360d55beb60c97588dd
Lori Culberson <bigskybandit@sbcglobal.net>
2020-01-06 10:55:27 -0500

    Add greeting randomizer script

--- src/zsh/terminal-greeting.sh
+++ src/zsh/terminal-greeting.sh
@@ -1,1 +1,14 @@
-echo "Hey there!"
+#!/bin/bash
+
+# create array of phrases
+# set variable for random one selected
+# echo variable
+
+greetings=("Have a fantastic day!" "Hello gorgeous!" "What are you coding today?" "Let’s have some fun!" "Have I told you how much I love you?" "You look marvelous!" "Good morning sunshine!")
+FLOOR=1;
+CEILING=7
+
+result=$(jot -r 1 $FLOOR $CEILING)
+greeting=${greetings[result]}
+# echo "greeting"
+echo greeting
