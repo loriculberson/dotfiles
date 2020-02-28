dce69246e6c43f3c90a645ebf0cc6855c7c40433
Lori Culberson <bigskybandit@sbcglobal.net>
2020-02-28 17:41:21 -0500

    Add random message to console

--- src/zsh/hello.js
+++ src/zsh/hello.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
-// const sayHi = () => console.log("Lori is amazing!")
+const options = [
+  "Yes Queen!", 
+  "Yabba dabba dooo!",
+  "Texas loves you",
+  "You are an IRONMAN!"
+]
 
-// sayHi()
+function getGreeting(greetings) {
+  const randNum = Math.floor(Math.random() * (4)) + 0; //The maximum is inclusive and the minimum is inclusive 
+  console.log(greetings[randNum])
+}
+
+getGreeting(options)
