9f4b73aa8495b7bba6be22520f5b0aa54bcbc536
Lori Culberson <bigskybandit@sbcglobal.net>
2020-05-21 16:16:32 -0500

    update console greetings

--- src/zsh/hello.js
+++ src/zsh/hello.js
@@ -5,11 +5,16 @@ const options = [
   "Yabba dabba dooo!",
   "Texas loves you",
   "You are an IRONMAN!",
-  "I'm too sexy for this terminal"
+  "I'm too sexy for this terminal",
+  "You look marvelous!",
+  "Kick ass!",
+  "We need more power!",
+  "There's no crying in baseball!",
+  "A hug is always the right size."
 ]
 
 function getGreeting(greetings) {
-  const randNum = Math.floor(Math.random() * (4)) + 0; //The maximum is inclusive and the minimum is inclusive 
+  const randNum = Math.floor(Math.random() * (10)); 
   console.log(greetings[randNum])
 }
 
